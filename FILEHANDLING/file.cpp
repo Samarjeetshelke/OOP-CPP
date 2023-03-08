59fecb298d76637d76669122c37696b2de87648f
Samarjeet Shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2023-03-08 14:52:46 +0530

    Add files via upload

---
+++ FILEHANDLING/file.cpp
@@ -0,0 +1,26 @@
+#include<fstream>
+#include<iostream>
+using namespace std;
+
+main(){
+    char data[100];
+   
+    ofstream obj;//The writing obj
+    ifstream obj2;//The reading obj
+
+        obj.open("test.txt");
+        cout<<"\nEnter Your name: ";
+        cin>>data;
+        obj<<data<<endl;//write the data in file from data[]
+        obj.close();
+
+        obj2.open("test.txt");
+        cout<<"\nName: ";
+        obj2>>data;//read the data from file to data[]
+        cout<<data;
+        obj2.close();
+
+        
+   
+    
+}
