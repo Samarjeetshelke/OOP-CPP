a0f4263a6b9c7dc74cb46ae6fc1f167a9aea7640
Samarjeet Shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2023-03-16 21:09:02 +0530

    Add files via upload

---
+++ CLASS & OBJECT/class.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+class Rectangle{
+    public://specifier
+
+        int height;//data member
+        int width;
+
+        int getWidth(){//member function
+            return this->width;
+        }
+
+        void setData(){
+            cout<<"\nEnter the Width: ";
+            cin>>this->width;
+
+            cout<<"\nEnter the Height: ";
+            cin>>this->height;
+        }
+        int getHeight(){
+            return this->height;
+        }
+
+        int area(int height,int width);
+};
+
+
+int Rectangle::area(int height,int width){
+    return height*width;
+}
+
+int main(){
+    Rectangle obj;
+    obj.setData();
+   // cout<<obj;
+    cout<<"\nThe Height of Rectangle is "<<obj.getHeight();
+    cout<<"\nThe Width of Rectangle is "<<obj.getWidth();
+    cout<<"\nThe Area of Rectangle is "<<obj.area(obj.height,obj.width);
+
+    return 0;
+}
