da94cf84b7c4e7e449e5545edbb906129dfe43ee
Samarjeet Shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2024-01-23 11:34:34 +0530

    Update decitonbase.cpp

--- ECT/decitonbase.cpp
+++ ECT/decitonbase.cpp
@@ -35,3 +35,45 @@ int main() {
    
    return 0;
 }
+
+
+
+///using string
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+string DectoNBase(int n ,int num){
+    string s;
+    char d;
+    int i=0;
+    
+    while(num){
+     d = num%n;
+     s+=d<10?(d+48):(d+55);
+     i++;
+     num/=n;
+    }
+    //i--;
+    /*while(i>=0){
+        cout<<s[i];
+        i--;
+    }*/
+    reverse(s.begin(),s.end());
+    return s;
+}
+int main() {
+    // Write C++ code here
+   int num,n;
+    
+   cin>>n;
+   cin>>num;
+   cout<<"Ans : ";
+   cout<<DectoNBase(n,num);
+   
+   //cout<<(s+1)<<(s+0);
+   
+   
+   return 0;
+}
