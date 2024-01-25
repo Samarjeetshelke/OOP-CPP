368194ce26ac81591fb02c316db1915b183296c6
Samarjeet Shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2024-01-25 11:51:50 +0530

    Update bridegroompair.cpp

--- ECT/bridegroompair.cpp
+++ ECT/bridegroompair.cpp
@@ -38,3 +38,45 @@ int main() {
 
     return 0;
 }
+
+
+///normal logic but not easy
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    string a;
+    string b;
+    int n;
+    
+    int i=0;
+    int left=n;
+    int flg =0;
+    
+    cin>>n;
+    cin>>a;
+    cin>>b;
+    
+    while(i<n && flg<left)
+    {
+        if(a[i]==b[i]){
+            i++;
+            left=n-i;
+            flg=0;
+        }
+        else{
+            flg++;
+            char c = b[i];
+            for(int j=i+1;j<n;j++){
+                b[j-1] = b[j];
+            }
+            b[n-1]=c;
+        }
+    }
+    
+    cout<<"Ans :"<<left;
+
+    return 0;
+}
