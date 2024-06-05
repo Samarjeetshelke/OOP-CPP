37df69f549559c236585621aa2426e138cf090cc
Samarjeet Shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2024-06-05 12:45:17 +0530

    Create Number_of_Subarray_with_samenumberof1sand0s.cpp

---
+++ HASHMAPS/Number_of_Subarray_with_samenumberof1sand0s.cpp
@@ -0,0 +1,33 @@
+class Solution{
+  public:
+    //Function to count subarrays with 1s and 0s.
+    long long int countSubarrWithEqualZeroAndOne(int arr[], int n)
+    {
+        //Your code here
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        int sum=0;
+        long long ans=0;
+        
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                sum -= 1;
+            }
+            else{
+                sum += 1;
+            }
+            
+            
+            if(mp.find(sum)!=mp.end()){
+                ans += mp[sum];
+                mp[sum]++;
+            }
+            else{
+                mp[sum] = 1;
+            }
+        }
+        
+        return ans;
+    }
+};
