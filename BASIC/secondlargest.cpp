df8e488533dc278a7e02061f9ae4c8ab0fef24eb
Samarjeet Shelke <59022780+Samarjeetshelke@users.noreply.github.com>
2024-07-11 15:40:14 +0530

    Update secondlargest.cpp

--- BASIC/secondlargest.cpp
+++ BASIC/secondlargest.cpp
@@ -30,3 +30,35 @@ main(){
 
 
 }
+
+
+//more proper code also consider the negative numbers
+// Online C++ compiler to run C++ program online
+#include<iostream>
+#include<limits.h>
+
+using namespace std;
+
+int main() {
+    
+    int arr[] = {-4,-5,-78,-9,-87,-90};
+    int max1=INT_MIN,max2=INT_MIN;
+    
+    for(int i : arr){
+        if(i>max1){
+            max2 = max1;
+            max1 = i;
+        }
+        else{
+            if(i>max2){
+                max2 = i;
+            }
+        }
+    }
+    
+    
+    cout<<"The First Large: "<<max1<<"\nThe Second Largest :"<<max2;
+    
+
+    return 0;
+}
